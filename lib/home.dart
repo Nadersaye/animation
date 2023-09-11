454f12ffabc1fcd71386dc295479285938868cd4
Nader <nsyd1582@gmail.com>
2023-09-11 18:53:15 +0300

    refactor project &create explicit animtion folder

--- lib/home.dart
+++ lib/home.dart
@@ -1,16 +1,16 @@
-import 'package:animation/Animation/animated_align.dart';
-import 'package:animation/Animation/animated_opacity.dart';
-import 'package:animation/Animation/animated_text_style.dart';
 import 'package:flutter/material.dart';
 
-import 'Animation/animated_container.dart';
-import 'Animation/animated_crossfade.dart';
-import 'Animation/animated_liststate.dart';
-import 'Animation/animated_padding.dart';
-import 'Animation/animated_physical.dart';
-import 'Animation/animated_positioned.dart';
-import 'Animation/animated_positioned_directional.dart';
-import 'Animation/animated_switcher.dart';
+import 'Implicit_Animation/animated_align.dart';
+import 'Implicit_Animation/animated_container.dart';
+import 'Implicit_Animation/animated_crossfade.dart';
+import 'Implicit_Animation/animated_liststate.dart';
+import 'Implicit_Animation/animated_opacity.dart';
+import 'Implicit_Animation/animated_padding.dart';
+import 'Implicit_Animation/animated_physical.dart';
+import 'Implicit_Animation/animated_positioned.dart';
+import 'Implicit_Animation/animated_positioned_directional.dart';
+import 'Implicit_Animation/animated_switcher.dart';
+import 'Implicit_Animation/animated_text_style.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
