994c3848f6dc2c6fe99b9ba2ae2f6c7394bbd95f
Nader <nsyd1582@gmail.com>
2023-09-11 16:42:27 +0300

    create animate list state

--- lib/Animation/animated_liststate.dart
+++ lib/Animation/animated_liststate.dart
@@ -9,53 +9,75 @@ class AnimatedListStateExample extends StatefulWidget {
 }
 
 class _AnimatedListStateExampleState extends State<AnimatedListStateExample> {
-  double _radius = 20.0;
-  Color _color = Colors.grey;
-  double _width = 100;
-  double _height = 100;
-  bool _bigSize = false;
-
+  final List items = [];
+  final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
+  final ScrollController _scrollController = ScrollController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Animation container'),
+        title: const Text('Animation List state'),
         centerTitle: true,
       ),
-      body: Center(
-        child: GestureDetector(
-          onTap: () {
-            _bigSize = !_bigSize;
-            setState(() {
-              // _bigSize = !_bigSize;
-              editContainer(_bigSize);
-            });
-          },
-          child: AnimatedContainer(
-            duration: const Duration(milliseconds: 5500),
-            width: _width,
-            height: _height,
-            curve: Curves.bounceInOut,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(_radius), color: _color),
-            child: Image.asset('assets/images/jerry.png'),
-          ),
-        ),
+      body: AnimatedList(
+          controller: _scrollController,
+          key: _listKey,
+          itemBuilder: ((context, index, animation) {
+            return SlideTransition(
+              position: animation.drive(Tween<Offset>(
+                  begin: const Offset(1, 0), end: const Offset(0, 0))),
+              child: AnimatedListItem(
+                text: items[index],
+                onpressed: () {
+                  deleteItem(index);
+                },
+              ),
+            );
+          })),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addItem,
+        child: const Icon(Icons.add),
       ),
     );
   }
 
-  void editContainer(bool x) {
-    if (x == true) {
-      _radius = 40.0;
-      _color = Colors.orangeAccent;
-      _width = 300;
-      _height = 300;
-    } else {
-      _radius = 20.0;
-      _color = Colors.grey;
-      _width = 100;
-      _height = 100;
-    }
+  void addItem() {
+    dynamic index = items.length;
+    items.add('item ${index + 1}');
+    _listKey.currentState!.insertItem(index);
+    Future.delayed(const Duration(milliseconds: 300), () {
+      _scrollController.animateTo(_scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 100),
+          curve: Curves.bounceInOut);
+    });
+  }
+
+  void deleteItem(int index) {
+    var itemValue = items.removeAt(index);
+    _listKey.currentState!.removeItem(index, (context, animation) {
+      return SizeTransition(
+        sizeFactor: animation,
+        child: AnimatedListItem(
+          text: itemValue,
+        ),
+      );
+    });
+  }
+}
+
+class AnimatedListItem extends StatelessWidget {
+  const AnimatedListItem({super.key, required this.text, this.onpressed});
+  final String text;
+  final VoidCallback? onpressed;
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 7,
+      child: ListTile(
+        title: Text(text),
+        trailing:
+            IconButton(onPressed: onpressed, icon: const Icon(Icons.delete)),
+      ),
+    );
   }
 }
