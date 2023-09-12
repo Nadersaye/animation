592abdfd37ec0c06f69b943361e5de775c82f8ad
Nader <nsyd1582@gmail.com>
2023-09-12 13:21:20 +0300

    finish positioned directional  transition

--- lib/Explicit_Animation/positioned_directional_transition.dart
+++ lib/Explicit_Animation/positioned_directional_transition.dart
@@ -12,11 +12,27 @@ class _PositionedDirectionalTransitionExampleState
     extends State<PositionedDirectionalTransitionExample>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
+  late Animation<RelativeRect> _animation;
 
   @override
   void initState() {
     super.initState();
-    _controller = AnimationController(vsync: this);
+    _controller =
+        AnimationController(vsync: this, duration: const Duration(seconds: 3));
+    _animation = RelativeRectTween(
+      begin: RelativeRect.fromDirectional(
+          textDirection: TextDirection.ltr,
+          start: 0,
+          top: 0,
+          end: 0,
+          bottom: 0),
+      end: RelativeRect.fromDirectional(
+          textDirection: TextDirection.ltr,
+          start: 100,
+          top: 100,
+          end: 0,
+          bottom: 0),
+    ).animate(_controller);
   }
 
   @override
@@ -27,6 +43,48 @@ class _PositionedDirectionalTransitionExampleState
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Positioned Directional Transition Example'),
+        centerTitle: true,
+      ),
+      body: Stack(children: [
+        AnimatedBuilder(
+          animation: _controller,
+          builder: (BuildContext context, Widget? child) {
+            return PositionedDirectional(
+              start: _animation.value.left,
+              top: _animation.value.top,
+              end: _animation.value.right,
+              bottom: _animation.value.bottom,
+              child: Container(
+                width: 200,
+                height: 200,
+                color: Colors.grey,
+                child: Image.asset('assets/images/dog.png'),
+              ),
+            );
+          },
+        ),
+        Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      _controller.reset();
+                      _controller.forward();
+                    },
+                    child: const Icon(Icons.start)),
+                ElevatedButton(
+                    onPressed: () {
+                      _controller.reverse();
+                    },
+                    child: const Icon(Icons.close))
+              ],
+            ))
+      ]),
+    );
   }
 }
